6ad1d0978214bb1e331e6587b47cb8f93fdc89ee
unknown <chombobintiomar5gmail.com>
2026-02-19 21:13:10 +0300

    HTTP methods

--- main.py
+++ main.py
@@ -4,14 +4,43 @@
 # 3. You must define a HTTP method(GET,POST,PUT,DELETE,PATCH)
 # 4. You must define a status code(200,201,404,401,500)
 
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET"])
+allowed_methods = ["GET","POST","UPDATE","DELETE","PATCH"]
+user_list = []
+
+@app.route("/",methods = allowed_methods)
 def home():
-    return jsonify({"Flask API Version":"1.0"}),200
+    method = request.method.lower()
+    if method == "get":
+        return jsonify({"Flask API Version":"1.0"}),200
+    else:
+        return jsonify({"msg":"Method not allowed"}),405
+    
+@app.route("/users",methods=allowed_methods)
+def users():
+    try:
+        method = request.method.lower()
+        if method == "get":
+            return jsonify({"data":user_list}),200
+        elif method == "post":
+            data = request.get_json ()
+            if data["name"] == "" or data["location"] =="":
+                return jsonify({"msg":"name and location fields required"}),403
+            else:
+                user_list.append(data)
+                return jsonify({"msg":"Successfully added user."}),201
+        else:
+            return jsonify({"msg":"Method not allowed"}),405
+    except Exception as e:
+        return jsonify({"error":str(e)}),500
 
 
 
-app.run ()
+
+
+
+
+app.run (debug=True)
