209b5b075f8bb0c982aa1f900f6cf1a8d503e1ee
unknown <chombobintiomar5gmail.com>
2026-03-03 21:26:12 +0300

    vue

--- database.py
+++ database.py
@@ -1,17 +1,10 @@
-# SQLAlchemy is an ORM.Object relational mapping.
-# Helps execute queries using methods.
-# Define the table structure is classes/models.
+from flask_sqlalchemy import SQLAlchemy
 
-from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column
-from sqlalchemy import String
+db = SQLAlchemy()
 
-class Base(DeclarativeBase):
-    pass
-
-# Map Users table to user class
-class User(Base):
-    __tablename__="users"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(100))
-    location : Mapped[str] = mapped_column(String(100))
-    
+class User(db.Model):
+    ___tablename___='users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)    
+    email = db.Column(db.String(120),unique=True,nullable=False)
+    password = db.Column(db.String(255), nullable=False)
